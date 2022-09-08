c1f811c97f70d3944235d89fd55aacfa5134fd22
Sarvarjan Dosmetov <Sarvarjan.Dosmetov@UZAUTOMOTORS.COM>
2022-09-08 15:53:39 +0500

    Initial commit

---
+++ src/main/java/uz/digitalone/appgmuzbekistan/AppGmuzbekistanApplication.java
@@ -0,0 +1,13 @@
+package uz.digitalone.appgmuzbekistan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGmuzbekistanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppGmuzbekistanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/digitalone/appgmuzbekistan/AppGmuzbekistanApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.digitalone.appgmuzbekistan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGmuzbekistanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
